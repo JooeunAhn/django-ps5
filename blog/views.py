fb7d0312eb7368831fefaa118f0a1a08e8589c16
JooeunAhn <jflow@hanyang.ac.kr>
2016-07-20 17:05:49 +0900

    동영상을 mp3로 변환

--- blog/views.py
+++ blog/views.py
@@ -5,14 +5,26 @@ import youtube_dl
 def index(request):
     return render(request, 'blog/index.html', {})
 
+def my_hook(d):
+    if d['status'] == "finished":
+        print("Done downloading, now convertiong ...")
+
 def serve(request):
     if request.method == "POST":
         form = AudioForm(request.POST)
         print(form.is_valid())
         if form.is_valid():
-
             form = form.save(commit=False)
-            with youtube_dl.YoutubeDL() as ydl:
+            ydl_opt = {
+                    'format':'bestaudio/best',
+                    'postprocessors':[{
+                        'key':'FFmpegExtractAudio',
+                        'preferredcodec':'mp3',
+                        'preferredquality':'192',
+                        }],
+                    'progress_hooks': [my_hook],
+                    }
+            with youtube_dl.YoutubeDL(ydl_opt) as ydl:
                 mp3 = ydl.download([form.url])
             Audio.objects.create(title="first", mp3=mp3, url = form.url)
             form = AudioForm()
